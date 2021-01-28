34c88588cff039d27d8e51a93d56323d1d6f713c
Raphael Alampay <raphael.alampay@gmail.com>
2021-01-28 13:42:18 +0800

    added canny option. Resolves #3

--- pyobjpextractor/__main__.py
+++ pyobjpextractor/__main__.py
@@ -6,38 +6,55 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from lib.ss_obj_extractor import SsObjExtractor
+from lib.canny_obj_extractor import CannyObjExtractor
+
+WINDOW_NAME="OUTPUT"
 
 KEY_Q = 113
 KEY_J = 106
 KEY_K = 107
 
+MODE_CHOICES = [
+  "ss",
+  "canny"
+]
+
+def mouse_callback(event, x, y, flags, param):
+  print("Current Position (%d, %d)" % (x, y))
+
 def main():
   parser = argparse.ArgumentParser(description="PyObjPExtractor: Object Proposal Extractor program")
 
+  parser.add_argument("--mode", help="Mode of object proposal extractor", type=str, default="ss", choices=MODE_CHOICES)
   parser.add_argument("--input-file", help="Input image file (jpg/jpeg only)", required=True, type=str)
   parser.add_argument("--output-dir", help="Output directory of saved image proposals", type=str, default="./")
-  parser.add_argument("--ss-rect-increment", help="Increment value for SS", type=int, default=1)
+  parser.add_argument("--rect-increment", help="Increment value for num rects", type=int, default=1)
+  parser.add_argument("--num-rects", help="Number of initial bounding boxes", type=int, default=500)
   parser.add_argument("--ss-padding", help="Padding in pixels for drawing rectangles", type=int, default=5)
   parser.add_argument("--ss-is-fast", help="Fast processing for SS", type=bool, default=True)
-  parser.add_argument("--ss-num-rects", help="Number of initial rectangles for SS", type=int, default=500)
 
   args = parser.parse_args()
 
-  input_file        = args.input_file
-  output_dir        = args.output_dir
-  ss_rect_increment = args.ss_rect_increment
-  ss_padding        = args.ss_padding
-  ss_is_fast        = args.ss_is_fast
-  ss_num_rects      = args.ss_num_rects
+  mode            = args.mode
+  input_file      = args.input_file
+  output_dir      = args.output_dir
+  rect_increment  = args.rect_increment
+  num_rects       = args.num_rects
+  ss_padding      = args.ss_padding
+  ss_is_fast      = args.ss_is_fast
 
   image = cv2.imread(input_file)
 
-  extractor = SsObjExtractor(img=image, padding=ss_padding, is_fast=ss_is_fast, num_rects=ss_num_rects)
+  if mode == "ss":
+    extractor = SsObjExtractor(img=image, padding=ss_padding, is_fast=ss_is_fast, num_rects=num_rects)
+  elif mode == "canny":
+    extractor = CannyObjExtractor(img=image, padding=ss_padding, num_rects=num_rects)
 
   extractor.exec()
 
   while True:
-    cv2.imshow("Output", extractor.processed_img)
+    cv2.imshow(WINDOW_NAME, extractor.processed_img)
+    cv2.setMouseCallback(WINDOW_NAME, mouse_callback)
     
     # Record key press
     k = cv2.waitKey(0) & 0xFF
@@ -48,17 +65,17 @@ def main():
 
     # Press 'j' to increase number of rectangles
     elif(k == KEY_J):
-      if(extractor.num_rects + ss_rect_increment > len(extractor.rects)):
+      if(extractor.num_rects + rect_increment > len(extractor.rects)):
         extractor.num_rects = len(extractor.rects)
       else:
-        extractor.num_rects += ss_rect_increment
+        extractor.num_rects += rect_increment
       print("Increasing regions to {}...".format(extractor.num_rects))
 
       extractor.draw_rectangles()
 
     # Press 'k' to decrease number of rectangles
-    elif(k == KEY_K and extractor.num_rects > ss_rect_increment):
-      extractor.num_rects -= ss_rect_increment
+    elif(k == KEY_K and extractor.num_rects > rect_increment):
+      extractor.num_rects -= rect_increment
       extractor.draw_rectangles()
       print("Decreasing regions to {}...".format(extractor.num_rects))
       
---
+++ pyobjpextractor/lib/canny_obj_extractor.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+class CannyObjExtractor:
+  def __init__(self, img=None, sigma=0.33, padding=5, sort_reverse=False, num_rects=100):
+    self.img          = img
+    self.sigma        = sigma
+    self.padding      = padding
+    self.sort_reverse = sort_reverse
+    self.num_rects    = num_rects
+
+  def exec(self):
+    self.gray   = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY) 
+    self.median = np.median(self.gray)
+
+    self.lower  = int(max(0, (1.0 - self.sigma) * self.median))
+    self.upper  = int(min(255, (1.0 + self.sigma) * self.median))
+    self.edged  = cv2.Canny(self.gray, self.lower, self.upper)
+
+    self.rects = self.process()
+
+    self.rects = sorted(self.rects, key=lambda x: x[2] * x[3], reverse=self.sort_reverse)
+
+    self.draw_rectangles()
+
+  def process(self):
+    contours, hierarchy = cv2.findContours(self.edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+
+    rects = []
+
+    for c in contours:
+      epsilon = 0.1*cv2.arcLength(c, True)
+      poly    = cv2.approxPolyDP(c, epsilon, True)
+
+      rects.append(cv2.boundingRect(poly))
+
+    return rects
+
+  def draw_rectangles(self):
+    # draw rectangles
+    self.processed_img = self.img.copy()
+
+    for i, rect in enumerate(self.rects):
+      if(i < self.num_rects):
+        x, y, w, h = rect
+        cv2.rectangle(self.processed_img, (x - self.padding, y - self.padding), (x + w + self.padding, y + h + self.padding), (0, 255, 0), 1, cv2.LINE_AA)
